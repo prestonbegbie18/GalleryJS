736d179e6381986abb491cef88c4c9cd27573aff
Unknown <pbegbie@mail.csuchico.edu>
2017-04-26 22:36:25 -0700

    Finished to the point of no duplicate locations

--- gallery.js
+++ gallery.js
@@ -1,12 +1,16 @@
 var chico = ["Bidwell Mansion","Chico State","Bidwell Park","Gateway Science Museum","National Yo-Yo Museum"];
+var current;
 
 function shuffleGallery()
 {
   var randIndex;
-  var location;
   randIndex = randomUpTo(chico.length - 1);
-  location = chico[randIndex];
-  console.log(location);
+  while(current == chico[randIndex])
+  {
+    randIndex = randomUpTo(chico.length - 1);
+  }
+  current = chico[randIndex];
+  console.log(current);
 }
 
 function randomUpTo(max)
