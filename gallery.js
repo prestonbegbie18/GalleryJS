c8ede8d13ddcdc22bf44f77d0272b236b057d8ea
Unknown <pbegbie@mail.csuchico.edu>
2017-04-26 21:08:17 -0700

    Finished the shuffleGallery function

--- gallery.js
+++ gallery.js
@@ -1,6 +1,15 @@
-
+var chico = ["Bidwell Mansion","Chico State","Bidwell Park","Gateway Science Museum","National Yo-Yo Museum"];
 
 function shuffleGallery()
 {
+  var randIndex;
+  var location;
+  randIndex = randomUpTo(chico.length - 1);
+  location = chico[randIndex];
+  console.log(location);
+}
 
+function randomUpTo(max)
+{
+  return Math.floor(Math.random() * (max+1));
 }
